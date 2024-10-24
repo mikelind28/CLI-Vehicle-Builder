351514d737434b2d79310cbe261990b7c0af8765
Mike Lind <mikelind28@gmail.com>
2024-10-23 19:36:45 -0500

    first edits

--- Develop/src/classes/Cli.ts
+++ Develop/src/classes/Cli.ts
@@ -7,15 +7,15 @@ import Wheel from "./Wheel.js";
 
 // define the Cli class
 class Cli {
-  // TODO: update the vehicles property to accept Truck and Motorbike objects as well
-  // TODO: You will need to use the Union operator to define additional types for the array
-  // TODO: See the AbleToTow interface for an example of how to use the Union operator
-  vehicles: (Car)[];
+  // TODO: update the vehicles property to accept Truck and Motorbike objects as well ✅
+  // TODO: You will need to use the Union operator to define additional types for the array ✅
+  // TODO: See the AbleToTow interface for an example of how to use the Union operator ✅
+  vehicles: ( Car | Truck | Motorbike )[];
   selectedVehicleVin: string | undefined;
   exit: boolean = false;
 
-  // TODO: Update the constructor to accept Truck and Motorbike objects as well
-  constructor(vehicles: (Car)[]) {
+  // TODO: Update the constructor to accept Truck and Motorbike objects as well ✅
+  constructor(vehicles: ( Car | Truck | Motorbike )[]) {
     this.vehicles = vehicles;
   }
 
@@ -60,16 +60,22 @@ class Cli {
           type: 'list',
           name: 'vehicleType',
           message: 'Select a vehicle type',
-          // TODO: Update the choices array to include Truck and Motorbike
-          choices: ['Car'],
+          // TODO: Update the choices array to include Truck and Motorbike ✅
+          choices: ['Car', 'Truck', 'Motorbike'],
         },
       ])
       .then((answers) => {
         if (answers.vehicleType === 'Car') {
           // create a car
           this.createCar();
+        } else if (answers.vehicleType === 'Truck') {
+          // create a truck
+          this.createTruck();
+        } else if (answers.vehicleType === 'Motorbike') {
+          // create a motorbike
+          this.createMotorbike();
         }
-        // TODO: add statements to create a truck or motorbike if the user selects the respective vehicle type
+        // TODO: add statements to create a truck or motorbike if the user selects the respective vehicle type ✅
       });
   }
 
@@ -170,10 +176,28 @@ class Cli {
         },
       ])
       .then((answers) => {
-        // TODO: Use the answers object to pass the required properties to the Truck constructor
-        // TODO: push the truck to the vehicles array
-        // TODO: set the selectedVehicleVin to the vin of the truck
-        // TODO: perform actions on the truck
+        const truck = new Truck(
+          // TODO: The generateVin method is static and should be called using the class name Cli, make sure to use Cli.generateVin() for creating a truck and motorbike as well! ✅
+          Cli.generateVin(),
+          answers.color,
+          answers.make,
+          answers.model,
+          parseInt(answers.year),
+          parseInt(answers.weight),
+          parseInt(answers.topSpeed),
+          parseInt(answers.towingCapacity),
+          []
+        );
+        // push the truck to the vehicles array
+        this.vehicles.push(truck);
+        // set the selectedVehicleVin to the vin of the truck
+        this.selectedVehicleVin = truck.vin;
+        // perform actions on the car
+        this.performActions();
+        // TODO: Use the answers object to pass the required properties to the Truck constructor ✅
+        // TODO: push the truck to the vehicles array ✅
+        // TODO: set the selectedVehicleVin to the vin of the truck ✅
+        // TODO: perform actions on the truck ✅
       });
   }
 
